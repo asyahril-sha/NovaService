b3a5d6f004d9f2678f39d7f6a84cdf7a0bfa98b0
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-28 12:38:03 +0800

    Create __init__.py

---
+++ characters/therapist/__init__.py
@@ -0,0 +1,25 @@
+# characters/therapist/__init__.py
+"""
+NovaService Therapist Characters Package
+8 karakter therapist
+"""
+
+# from characters.therapist.anya import AnyaCharacter
+# from characters.therapist.syifa import SyifaCharacter
+# from characters.therapist.laura import LauraCharacter
+# from characters.therapist.tara import TaraCharacter
+# from characters.therapist.pevita import PevitaCharacter
+# from characters.therapist.maudy import MaudyCharacter
+# from characters.therapist.zara import ZaraCharacter
+# from characters.therapist.angela import AngelaCharacter
+
+__all__ = [
+    # 'AnyaCharacter',
+    # 'SyifaCharacter',
+    # 'LauraCharacter',
+    # 'TaraCharacter',
+    # 'PevitaCharacter',
+    # 'MaudyCharacter',
+    # 'ZaraCharacter',
+    # 'AngelaCharacter',
+]
