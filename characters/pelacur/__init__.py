3b14a234ee4baced2e3d84f47b70fe5ca3ca9d20
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-28 12:38:22 +0800

    Create __init__.py

---
+++ characters/pelacur/__init__.py
@@ -0,0 +1,25 @@
+# characters/pelacur/__init__.py
+"""
+NovaService Pelacur Characters Package
+8 karakter pelacur
+"""
+
+# from characters.pelacur.davina import DavinaCharacter
+# from characters.pelacur.michelle import MichelleCharacter
+# from characters.pelacur.jihane import JihaneCharacter
+# from characters.pelacur.tissa import TissaCharacter
+# from characters.pelacur.hana import HanaCharacter
+# from characters.pelacur.shindy import ShindyCharacter
+# from characters.pelacur.nadya import NadyaCharacter
+# from characters.pelacur.alyssa import AlyssaCharacter
+
+__all__ = [
+    # 'DavinaCharacter',
+    # 'MichelleCharacter',
+    # 'JihaneCharacter',
+    # 'TissaCharacter',
+    # 'HanaCharacter',
+    # 'ShindyCharacter',
+    # 'NadyaCharacter',
+    # 'AlyssaCharacter',
+]
