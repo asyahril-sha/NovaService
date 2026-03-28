378782ea4fe5e300a4d0fe58c429e02ac4ac1fce
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-29 05:21:18 +0800

    Create pelacur_flow.py

---
+++ service/pelacur_flow.py
@@ -0,0 +1,9 @@
+# service/pelacur_flow.py
+"""
+Pelacur Flow - Main entry point
+Wrapper untuk memudahkan import seperti therapist_flow
+"""
+
+from service.pelacur_system import PelacurSystem as PelacurFlow
+
+__all__ = ['PelacurFlow']
