4e0a12f487b208caf301e7a99b5070e46431c52c
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-28 12:37:40 +0800

    Create __init__.py

---
+++ characters/__init__.py
@@ -0,0 +1,49 @@
+# characters/__init__.py
+"""
+NovaService Characters Package
+16 karakter: 8 therapist, 8 pelacur
+"""
+
+from characters.base_character import BaseCharacter
+
+# Therapist characters (akan diisi setelah file karakter dibuat)
+# from characters.therapist.anya import AnyaCharacter
+# from characters.therapist.syifa import SyifaCharacter
+# from characters.therapist.laura import LauraCharacter
+# from characters.therapist.tara import TaraCharacter
+# from characters.therapist.pevita import PevitaCharacter
+# from characters.therapist.maudy import MaudyCharacter
+# from characters.therapist.zara import ZaraCharacter
+# from characters.therapist.angela import AngelaCharacter
+
+# Pelacur characters
+# from characters.pelacur.davina import DavinaCharacter
+# from characters.pelacur.michelle import MichelleCharacter
+# from characters.pelacur.jihane import JihaneCharacter
+# from characters.pelacur.tissa import TissaCharacter
+# from characters.pelacur.hana import HanaCharacter
+# from characters.pelacur.shindy import ShindyCharacter
+# from characters.pelacur.nadya import NadyaCharacter
+# from characters.pelacur.alyssa import AlyssaCharacter
+
+__all__ = [
+    'BaseCharacter',
+    # Therapist
+    # 'AnyaCharacter',
+    # 'SyifaCharacter',
+    # 'LauraCharacter',
+    # 'TaraCharacter',
+    # 'PevitaCharacter',
+    # 'MaudyCharacter',
+    # 'ZaraCharacter',
+    # 'AngelaCharacter',
+    # Pelacur
+    # 'DavinaCharacter',
+    # 'MichelleCharacter',
+    # 'JihaneCharacter',
+    # 'TissaCharacter',
+    # 'HanaCharacter',
+    # 'ShindyCharacter',
+    # 'NadyaCharacter',
+    # 'AlyssaCharacter',
+]
